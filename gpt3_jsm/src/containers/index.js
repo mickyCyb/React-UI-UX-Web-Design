0a91c8fa8cc3e0b0365a8037703ae1321cca9e27
Michael Gashaw <michael@peoplise.com>
2022-06-06 22:17:29 +0300

    created folder and files

---
+++ gpt3_jsm/src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog } from './blog/blog';
+export {default as Feature} from './features/features';
+export {default as Footer} from './footer/footer';
+export {default as Header} from './header/header';
+export {default as Possibility} from './possibility/possibility';
+export {default as WhatGPT3} from './whatGPT3/whatgpt3'
